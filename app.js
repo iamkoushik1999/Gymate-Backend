51d53a9598ad9c601f39f504f6b7e0c19ea93167
iamkoushik1999 <iamkoushik1999@gmail.com>
2025-06-13 23:44:59 +0530

    Feat: added swagger

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
+// Emulate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 // Database
 import connectDB from './config/database.js';
 connectDB();
@@ -22,5 +29,26 @@ app.use(cookieParser());
 app.use(routes);
 
 app.use(errorHandler);
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Gymate API Docs',
+      version: '1.0.0',
+      description: 'API Docs',
+    },
+    server: [
+      {
+        url: 'gymate-backend.onrender.com/api/v1',
+      },
+    ],
+  },
+  apis: [path.join(__dirname, './swagger.yaml')],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 export default app;
