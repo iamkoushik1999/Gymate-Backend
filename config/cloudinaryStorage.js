d2bcb80ccc1cbc2c038946fa997b5199b4c4f162
iamkoushik1999 <iamkoushik1999@gmail.com>
2025-06-16 22:51:47 +0530

    Feat: Cloudinary setup

---
+++ config/cloudinaryStorage.js
@@ -0,0 +1,14 @@
+// config/cloudinaryStorage.mjs
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from './cloudinary.js';
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'gym-mate',
+    allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }],
+  },
+});
+
+export default storage;
